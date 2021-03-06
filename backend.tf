15e837aaa2b41e8bc5d13f0482900d5d49b8909f
Terry Lowery <tl@lowerytech.net>
2021-03-06 08:08:43 +0000

    added networks and modified s3 backend

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ terraform {
     region  = "us-east-1"
     profile = "default"
     key     = "terraformstatefile"
-    bucket  = "terraformstatebucket01121"
+    bucket  = "terraformstatebucket12345"
   }
 }
