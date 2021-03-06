109b875548ac63b2a2422dbbc94e874e6a67febb
Terry Lowery <tl@lowerytech.net>
2021-03-06 17:39:35 -0600

    Updated and added routing table

--- networks.tf
+++ networks.tf
@@ -66,3 +66,72 @@ resource "aws_subnet" "subnet_1_oregon" {
   vpc_id     = aws_vpc.vpc_master_oregon.id
   cidr_block = "192.168.1.0/24"
 }
+
+# ********   Peering setups ********* #
+resource "aws_vpc_peering_connection" "useast1-uswest2" {
+  provider    = aws.region-master
+  peer_vpc_id = aws_vpc.vpc_master_oregon.id
+  vpc_id      = aws_vpc.vpc_master.id
+  peer_region = var.region-worker
+}
+
+#Accept VPC peering request in us-west-2 from us-east-1
+resource "aws_vpc_peering_connection_accepter" "accept_peering" {
+  provider                  = aws.region-worker
+  vpc_peering_connection_id = aws_vpc_peering_connection.useast1-uswest2.id
+  auto_accept               = true
+}
+
+# ********   Route tables setups ********* #
+#Create route table in us-east-1
+resource "aws_route_table" "internet_route" {
+  provider = aws.region-master
+  vpc_id   = aws_vpc.vpc_master.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  route {
+    cidr_block                = "192.168.1.0/24"
+    vpc_peering_connection_id = aws_vpc_peering_connection.useast1-uswest2.id
+  }
+  lifecycle {
+    ignore_changes = all
+  }
+  tags = {
+    Name = "Master-Region-RT"
+  }
+}
+
+#Overwrite default route table of VPC(Master) with our route table entries
+resource "aws_main_route_table_association" "set-master-default-rt-assoc" {
+  provider       = aws.region-master
+  vpc_id         = aws_vpc.vpc_master.id
+  route_table_id = aws_route_table.internet_route.id
+}
+
+#Create route table is us-west-2
+resource "aws_route_table" "internet_route_oregon" {
+  provider = aws.region-worker
+  vpc_id   = aws_vpc.vpc_master_oregon.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw-oregon.id
+  }
+  route {
+    cidr_block                = "10.0.1.0/24"
+    vpc_peering_connection_id = aws_vpc_peering_connection.useast1-uswest2.id
+  }
+  lifecycle {
+    ignore_changes = all
+  }
+  tags = {
+    Name = "Worker-Region-RT"
+  }
+}
+#Overwrite default route table of VPC(Scondary) with out route dtable entries
+resource "aws_main_route_table_association" "set-master-default-rt-assoc" {
+  provider       = aws.region-worker
+  vpc_id         = aws_vpc.vpc_master_oregon.id
+  route_table_id = aws_route_table.internet_route.id
+}
